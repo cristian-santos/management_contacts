5bbf9d01b2d2654194e042d2b1a9b7800092eb01
cristian-santos <cristiansantosti@gmail.com>
2022-11-19 10:16:00 -0300

    create table contact with migration

---
+++ database/migrations/2022_11_19_130702_create_contact.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContact extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100)->nullable();
+            $table->string('contact', 9)->unique()->nullable();
+            $table->string('email', 25)->unique()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact');
+    }
+}
